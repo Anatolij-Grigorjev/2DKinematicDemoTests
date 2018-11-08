dbcb4baf79493b313f4314996a811d41893dc6d6
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-11-08 13:59:35 +0200

    initial chain move abstract script for extension

---
+++ game/abstract_chain_move.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+var is_performing = false #is the move being performed right now
+var next_valid_actions = ["attack1", "attack2"]
+
+#dynamic atomic variables
+var selected_next_action = "" #next pressed action to begin after this one is over
+var anim = "" #animation to play during the move
+var move_duration = 1.0 #actual time it takes to complete the move
+
+#dynamic node references
+var char_root #character root node
+var animator #animation player to insert move animation
+var hitbox #hitbox rectangel for signals
+var next_chain_a1 #next chain member on attack1, node
+var next_chain_a2 #next chain member on attack2, node
+var timer #timer type for timeout of combo move
+
+func _ready():
+	init_move_vars()
+	timer.wait_time = move_duration
+	timer.connect("timeout", self, "_on_move_time_end")
+	timer.stop()
+	pass
+
+func _process(delta):
+	#while the move is hapening we can try set which move comes next
+	if (is_performing):
+		for action in next_valid_actions:
+			if (Input.is_key_pressed(action)):
+				selected_next_action = action
+	pass
+
+#starts combo move sequence. 
+#all combo moves need to implement this method
+func begin():
+	timer.start()
+	is_performing = true
+	animator.play(anim)
+	hitbox.enabled = true
+	pass
+
+#child scripts need to override 
+#this and set initial move params
+func init_move_vars():
+	pass
+
+
+func _on_move_time_end():
+	
+	is_performing = false
+	hitbox.enabled = false
+	
+	#try continue selected attack chain
+	if (selected_next_action == "attack1" && next_chain_a1):
+		next_chain_a1.begin()
+	elif (selected_next_action == "attack2" && next_chain_a2):
+		next_chain_a2.begin()
+	else:
+		#none selected or available - signal end of combo
+		emit_signal("combo_over")
+	pass
