cee683a86a3e01c95a5b5ef3deee9d9de01921e1
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-11-10 13:44:37 +0200

    add combo move scenes

--- game/abstract_chain_move.gd
+++ game/abstract_chain_move.gd
@@ -11,24 +11,29 @@ var move_duration = 1.0 #actual time it takes to complete the move
 
 #dynamic node references
 var char_root #character root node
-var hitbox #hitbox rectangel for signals
+var hitbox #hitbox rectangle for signals
 var next_chain_a1 #next chain member on attack1, node
 var next_chain_a2 #next chain member on attack2, node
 var timer #timer type for timeout of combo move
+var char_animator #animator cached after mov started (to pass along)
+
+#combo finish signal
+signal combo_over
 
 func _ready():
 	init_move_vars()
 	hitbox_enabled = false
 	timer.wait_time = move_duration
-	timer.connect("timeout", self, "_on_move_time_end")
+	timer.one_shot = true
 	timer.stop()
+	timer.connect("timeout", self, "_on_move_time_end")
 	pass
 
 func _process(delta):
 	#while the move is hapening we can try set which move comes next
 	if (is_performing):
 		for action in next_valid_actions:
-			if (Input.is_key_pressed(action)):
+			if Input.is_action_just_pressed(action):
 				selected_next_action = action
 	pass
 
@@ -36,6 +41,7 @@ func _process(delta):
 #all combo moves need to have this method 
 #(and will if inhertinig from this script)
 func begin(animator):
+	char_animator = animator
 	timer.start()
 	is_performing = true
 	animator.play(anim)
@@ -54,11 +60,11 @@ func _on_move_time_end():
 	hitbox_enabled = false
 	
 	#try continue selected attack chain
-	if (selected_next_action == "attack1" && next_chain_a1):
-		next_chain_a1.begin()
-	elif (selected_next_action == "attack2" && next_chain_a2):
-		next_chain_a2.begin()
+	if (selected_next_action == "attack1" && next_chain_a1 != null):
+		next_chain_a1.begin(char_animator)
+	elif (selected_next_action == "attack2" && next_chain_a2 != null):
+		next_chain_a2.begin(char_animator)
 	else:
-		#none selected or available - signal end of combo
+		#none selected or selected not available - signal end of combo
 		emit_signal("combo_over")
 	pass
---
+++ game/combo_move1_2.gd
@@ -0,0 +1,7 @@
+extends "res://abstract_chain_move.gd"
+
+
+func init_move_vars():
+	anim = "attack1_2"
+	move_duration = 1.5
+	timer = $Timer
